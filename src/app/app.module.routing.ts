0944e07fd4fb91d18ffcfa364e2207b087118837
Himanshu Sharma <sharma9555@gmail.com>
2020-02-23 22:37:42 +0530

    login register and shared

---
+++ src/app/app.module.routing.ts
@@ -0,0 +1,11 @@
+import { RouterModule, Route } from '@angular/router';
+
+// const ROUTES: Route[] = [
+//   { path: 'home', component: HomeComponent },
+//   { path: 'notes',
+//     children: [
+//       { path: '', component: NotesComponent },
+//       { path: ':id', component: NoteComponent }
+//     ]
+//   },
+// ];
